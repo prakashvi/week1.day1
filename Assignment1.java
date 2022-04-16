43fca7694b05500f3bbe660b515381a396611057
prakashvi <103840796+prakashvi@users.noreply.github.com>
2022-04-16 20:52:26 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,26 @@
+package week1.days1;
+
+public class Assignment1 {
+public static void main(String[] args) {
+	int number=-40;
+	int number1=35;
+	if(number<0)
+	{
+		System.out.println("The number -40 is converted to 40");
+	}
+	
+	if(number1>0)
+	{
+		System.out.println(number1+ "is possitive");
+	}
+	else if(number1<0)
+	{
+		System.out.println(number1+ "is negative");
+	
+	}
+	else
+	{
+		System.out.println(number1+ "is neither positive nor negative");
+	}
+}
+}
